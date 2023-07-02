f8e060177aac1136d340632daca9cca2a58edda1
Gonzalo Escamilla <escamillagonzalo@gmail.com>
2023-07-02 16:04:14 -0300

    Folders cleanup

--- 2DCharacterController/res/resources.h
+++
@@ -1,7 +0,0 @@
-#ifndef _RES_RESOURCES_H_
-#define _RES_RESOURCES_H_
-
-extern const SpriteDefinition sprCharacter;
-extern const Image bgTile;
-
-#endif // _RES_RESOURCES_H_
--- 2DCharacterController/res/resources.res
+++
@@ -1,2 +0,0 @@
-IMAGE bgTile "tiles/bgtile.png" NONE
-SPRITE sprCharacter "sprites/Character2D.png" 4 4 NONE
